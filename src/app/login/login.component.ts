0b47ccb78bec549c710ff4603cb4044e3066bffe
enzomiguel7 <157727349+enzomiguel7@users.noreply.github.com>
2025-05-12 21:06:02 -0300

    enzo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,  // Importante para funcionar com loadComponent
-  imports: [CommonModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']  // Corrigido "styleUrl" para "styleUrls"
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent { }
+export class LoginComponent {
+  loginForm: FormGroup;
+  erroLogin: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      nome_usuario: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    this.http.post<any>('http://localhost:3000/login', this.loginForm.value)
+      .subscribe({
+        next: response => {
+          // Armazena token (mesmo que seja falso por enquanto)
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/receitas']);
+        },
+        error: err => {
+          this.erroLogin = err.error?.mensagem || 'Erro ao fazer login.';
+        }
+      });
+  }
+}
